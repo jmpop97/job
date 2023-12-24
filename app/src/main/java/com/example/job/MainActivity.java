e8b5d59972e182921fa377f56f116951ce7c0577
jmpop <pjm970128@gmail.com>
2023-12-25 03:07:55 +0900

    main setting

--- app/src/main/java/com/example/job/MainActivity.java
+++ app/src/main/java/com/example/job/MainActivity.java
@@ -2,13 +2,58 @@ package com.example.job;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("ver", "1");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //vars
+        Button site_button = findViewById(R.id.site);
+        Button problem_button = findViewById(R.id.problem);
+        Button solve_button = findViewById(R.id.solve);
+
+        site_button.setOnClickListener(site_listener);
+        problem_button.setOnClickListener(problem_listener);
+        solve_button.setOnClickListener(solve_listener);
+
     }
+
+    View.OnClickListener site_listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            intent =new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("https://cafe.naver.com/go2ndlife"));
+            startActivity(intent);
+        }
+    };
+    View.OnClickListener problem_listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+//            TextView phl = (TextView) findViewById(R.id.textView);
+//            int phlength=phl.getMeasuredWidth();
+            //            intent.putExtra("폰길이",phlength);
+
+            intent = new Intent(getApplicationContext(), problemActivity.class);
+            startActivity(intent);
+        }
+    };
+    View.OnClickListener solve_listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            intent = new Intent(getApplicationContext(), solveActivity.class);
+            startActivity(intent);
+        }
+    };
 }
---
+++ app/src/main/java/com/example/job/solveActivity.java
@@ -0,0 +1,13 @@
+package com.example.job;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class solveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solve);
+    }
+}
